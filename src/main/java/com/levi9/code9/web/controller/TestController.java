20928ac8978391e80562b5eac3900a927a61baeb
Srdjan Racic Beric <srle.025@gmail.com>
2015-10-18 22:54:53 +0200

    finished Test pages and logic

--- src/main/java/com/levi9/code9/web/controller/TestController.java
+++ src/main/java/com/levi9/code9/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.levi9.code9.web.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.levi9.code9.model.Question;
 import com.levi9.code9.model.Test;
 import com.levi9.code9.service.QuestionService;
 import com.levi9.code9.service.TestService;
@@ -68,5 +71,61 @@ public class TestController {
 	public String cancel() {
 		return "redirect:tests";
 	}
-
+	
+	@RequestMapping(value = "/editQuestions/{id}", method = RequestMethod.GET)
+	public String getAddRemoveQuestions(@PathVariable Long id, Model model) {
+		Test test = testService.findById(id);
+		List<Question> nonAssignedQuestions = questionService.findAll();
+		nonAssignedQuestions.removeAll(test.getQuestions());
+		List<Question> assignedQuestions = new ArrayList<>();
+		assignedQuestions.addAll(test.getQuestions());
+		model.addAttribute("test", id);
+		model.addAttribute("assignedQuestions", assignedQuestions);
+		model.addAttribute("nonAssignedQuestions", nonAssignedQuestions);
+		return "addRemoveQuestions";
+	}
+	
+	@RequestMapping(params = "addQuestion", method = RequestMethod.POST)
+	public String addQuestion(@RequestParam Long testId, 
+			@RequestParam(value = "nonAssignedQuestions", required = false, defaultValue = "-1") Long questionId, 
+			Model model) {
+		Question question = questionService.findById(questionId);
+		Test test = testService.findById(testId);
+		if (question != null) {
+			test.addQuestion(question);
+			testService.save(test);
+		} else {
+			model.addAttribute("addQuestionRequired", "test.addQuestion.required");
+		}
+		List<Question> nonAssignedQuestions = questionService.findAll();
+		nonAssignedQuestions.removeAll(test.getQuestions());
+		List<Question> assignedQuestions = new ArrayList<>();
+		assignedQuestions.addAll(test.getQuestions());
+		model.addAttribute("test", testId);
+		model.addAttribute("assignedQuestions", assignedQuestions);
+		model.addAttribute("nonAssignedQuestions", nonAssignedQuestions);
+		return "addRemoveQuestions";
+	}
+	
+	@RequestMapping(params = "removeQuestion", method = RequestMethod.POST)
+	public String removeQuestion(@RequestParam Long testId, 
+			@RequestParam(value = "assignedQuestions", required = false, defaultValue = "-1") Long questionId, 
+			Model model) {
+		Question question = questionService.findById(questionId);
+		Test test = testService.findById(testId);
+		if (question != null) {
+			test.removeQuestion(question);
+			testService.save(test);
+		} else {
+			model.addAttribute("removeQuestionRequired", "test.removeQuestion.required");
+		}
+		List<Question> nonAssignedQuestions = questionService.findAll();
+		nonAssignedQuestions.removeAll(test.getQuestions());
+		List<Question> assignedQuestions = new ArrayList<>();
+		assignedQuestions.addAll(test.getQuestions());
+		model.addAttribute("test", testId);
+		model.addAttribute("assignedQuestions", assignedQuestions);
+		model.addAttribute("nonAssignedQuestions", nonAssignedQuestions);
+		return "addRemoveQuestions";
+	}
 }
